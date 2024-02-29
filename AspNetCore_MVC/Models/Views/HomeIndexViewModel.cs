08aa4f94e72852485e50c8252ee69b036840e0f9
LucasWictor <126620359+LucasWictor@users.noreply.github.com>
2024-02-29 12:56:04 +0100

    +Minor logotype fix

--- AspNetCore_MVC/Models/Views/HomeIndexViewModel.cs
+++ AspNetCore_MVC/Models/Views/HomeIndexViewModel.cs
@@ -9,17 +9,17 @@ namespace AspNetCore_MVC.Models.Views
         public ShowcaseViewModel Showcase { get; set; } = new ShowcaseViewModel
         {
             Id = "overview",
-            ShowcaseImage = new ImageViewModel { ImageUrl = "images/taskmaster.svg", AltText = "Showcase image" },
+            ShowcaseImage = new ImageViewModel { ImageUrl = "/images/taskmaster.svg", AltText = "Showcase image" },
             Title = "Task Management Assistant You're Going to Love",
             Text = "We offer you a new generation of task management system. Plan, manage, and track all your tasks in one flexible tool.",
             link = new LinkViewModel { ControllerName = "Downloads", ActionName = "index", Text = "Get started for free" },
             BrandsText = "The largest companies use our tool to work efficiently",
             Brands = new List<ImageViewModel>
             {
-                new ImageViewModel { ImageUrl = "images/brands/brand_1.svg", AltText = "Brand Name 1" },
-                new ImageViewModel { ImageUrl = "images/brands/brand_2.svg", AltText = "Brand Name 2" },
-                new ImageViewModel { ImageUrl = "images/brands/brand_3.svg", AltText = "Brand Name 3" },
-                new ImageViewModel { ImageUrl = "images/brands/brand_4.svg", AltText = "Brand Name 4" }
+                new ImageViewModel { ImageUrl = "/images/brands/brand_1.svg", AltText = "Brand Name 1" },
+                new ImageViewModel { ImageUrl = "/images/brands/brand_2.svg", AltText = "Brand Name 2" },
+                new ImageViewModel { ImageUrl = "/images/brands/brand_3.svg", AltText = "Brand Name 3" },
+                new ImageViewModel { ImageUrl = "/images/brands/brand_4.svg", AltText = "Brand Name 4" }
             }
         };
     }
