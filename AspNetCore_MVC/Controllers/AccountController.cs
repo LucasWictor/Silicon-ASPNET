67af0f28b2a43ad75b13f7cb5428fd5b4399a08d
LucasWictor <126620359+LucasWictor@users.noreply.github.com>
2024-03-13 14:48:46 +0100

    Refactor Auth actions and add signup view and styles

--- AspNetCore_MVC/Controllers/AccountController.cs
+++ AspNetCore_MVC/Controllers/AccountController.cs
@@ -9,20 +9,5 @@ namespace AspNetCore_MVC.Controllers
             ViewData["Title"] = "Profile";
             return View();
         }
-        public IActionResult SignIn()
-        {
-            ViewData["Title"] = "Sign In";
-            return View();
-        }
-        public IActionResult SignUp()
-        {
-            ViewData["Title"] = "Sign Up";
-            return View();
-        }
-        public new IActionResult SignOut()
-        {
-
-            return RedirectToAction("Index", "Home");
-        }
     }
 }
---
+++ AspNetCore_MVC/Controllers/Authcontroller.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCore_MVC.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult SignIn()
+        {
+            ViewData["Title"] = "Sign In";
+            return View();
+        }
+
+        public IActionResult SignUp()
+        {
+            ViewData["Title"] = "Sign Up";
+            return View();
+        }
+
+        public IActionResult SignOut()
+        {
+            // logic here
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
