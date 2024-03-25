c98ead6ddefd3f1a4167116400c0603942ce51dd
LucasWictor <126620359+LucasWictor@users.noreply.github.com>
2024-03-25 14:05:22 +0100

    Signin  added

---
+++ AspNetCore_MVC/Models/SignInModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspNetCore_MVC.Models;
+
+public class SignInModel
+{
+    [Display(Name = "Email address", Prompt = "Enter your email address", Order = 0)]
+    [DataType(DataType.EmailAddress)]
+    [Required(ErrorMessage ="Email is required")]
+    public string Email { get; set; } = null!;
+
+    [DataType(DataType.Password)]
+    [Display(Name = "Password", Prompt = "Enter your password", Order = 1)]
+    [Required(ErrorMessage = "Password is required")]
+    public string Password { get; set; }
+
+
+    [Display(Name = "Remember me",  Order = 2)]
+    public bool RememberMe { get; set; }
+}
