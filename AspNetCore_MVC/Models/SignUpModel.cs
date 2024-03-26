488fe5eddc3ab8974f163fd9a94b238543b497f2
LucasWictor <126620359+LucasWictor@users.noreply.github.com>
2024-03-26 14:19:38 +0100

    +Added javascript support to Layout

--- AspNetCore_MVC/Models/SignUpModel.cs
+++ AspNetCore_MVC/Models/SignUpModel.cs
@@ -7,12 +7,12 @@ namespace AspNetCore_MVC.Models
     {
         [DataType(DataType.Text)]
         [Display(Name = "First name", Prompt = "Enter your first name", Order = 0)]
-        [Required(ErrorMessage = "Invalid first name")]
+        [Required(ErrorMessage = "First name must be provided")]
         public string FirstName { get; set; } = null!;
 
         [DataType(DataType.Text)]
         [Display(Name = "Last name", Prompt = "Enter your last name", Order = 1)]
-        [Required(ErrorMessage = "Invalid last name")]
+        [Required(ErrorMessage = "Last name must be provided")]
         public string LastName { get; set; } = null!;
 
         [DataType(DataType.EmailAddress)]
