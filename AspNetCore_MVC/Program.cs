21f4e458fa4f79364a880b951dd307bcf02c6180
LucasWictor <Dahlulle@gmail.com>
2024-04-04 00:52:30 +0200

    Added logging

--- AspNetCore_MVC/Program.cs
+++ AspNetCore_MVC/Program.cs
@@ -4,26 +4,34 @@ using Infrastructure.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Configure services
 builder.Services.AddControllersWithViews();
 builder.Services.AddRouting(x => x.LowercaseUrls = true);
 
 builder.Services.AddDbContext<DataContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("PostgresConnection")));
+
 builder.Services.AddScoped<AddressRepository>();
 builder.Services.AddScoped<UserRepository>();
 builder.Services.AddScoped<AddressService>();
 builder.Services.AddScoped<UserService>();
 
+/
+builder.Logging.AddConsole(); // Adds console logging
+builder.Logging.AddDebug(); // Adds debug window logging
 
 var app = builder.Build();
+
+// Configure middleware
 app.UseHsts();
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
 app.UseAuthorization();
+
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
-
