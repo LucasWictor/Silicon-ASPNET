364791b1d42eaa2fb001ca1c6066c310c33809fd
LucasWictor <Dahlulle@gmail.com>
2024-04-03 17:19:13 +0200

    Moved SignInModel to Infrastructure

--- Infrastructure/Models/SignInModel.cs
+++ Infrastructure/Models/SignInModel.cs
@@ -1,6 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
 
-namespace AspNetCore_MVC.Models;
+namespace Infrastructure.Models;
 
 public class SignInModel
 {
