5478d7c91c0f63f915e2f7f13e4f2656396885b5
LucasWictor <Dahlulle@gmail.com>
2024-04-03 22:39:33 +0200

    Fixed program startup

--- AspNetCore_MVC/Program.cs
+++ AspNetCore_MVC/Program.cs
@@ -1,4 +1,6 @@
 using Infrastructure.Contexts;
+using Infrastructure.Repositories;
+using Infrastructure.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,7 +8,12 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddRouting(x => x.LowercaseUrls = true);
 
 builder.Services.AddDbContext<DataContext>(options =>
-    options.UseSqlite(builder.Configuration.GetConnectionString("SQLiteConnection")));
+    options.UseNpgsql(builder.Configuration.GetConnectionString("PostgresConnection")));
+builder.Services.AddScoped<AddressRepository>();
+builder.Services.AddScoped<UserRepository>();
+builder.Services.AddScoped<AddressService>();
+builder.Services.AddScoped<UserService>();
+
 
 var app = builder.Build();
 app.UseHsts();
