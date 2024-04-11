346854dcad057d8efbac4253f5360a9391e172e1
LucasWictor <Dahlulle@gmail.com>
2024-04-11 13:27:51 +0200

    Added debug and console logging + Developer exception page and cookies.

--- AspNetCore_MVC/Program.cs
+++ AspNetCore_MVC/Program.cs
@@ -1,13 +1,18 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.EntityFrameworkCore;
 using Infrastructure.Contexts;
 using Infrastructure.Repositories;
 using Infrastructure.Services;
-using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Configure services
 builder.Services.AddControllersWithViews();
 builder.Services.AddRouting(x => x.LowercaseUrls = true);
+
+
 
 builder.Services.AddDbContext<DataContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("PostgresConnection")));
@@ -21,22 +26,47 @@ builder.Services.AddScoped<UserRepository>();
 builder.Services.AddScoped<AddressService>();
 builder.Services.AddScoped<UserService>();
 
+// Configure Authentication
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/signin";
+        options.LogoutPath = "/signout";
+        options.Cookie.HttpOnly = true;
+        options.Cookie.SecurePolicy = CookieSecurePolicy.SameAsRequest; 
+        options.Cookie.SameSite = SameSiteMode.Lax; 
+    });
 
-
-builder.Logging.AddConsole(); // Adds console logging
-builder.Logging.AddDebug(); // Adds debug window logging
+builder.Logging.AddConsole(); // console logging
+builder
+.Logging.AddDebug(); // debug window logging
 
 var app = builder.Build();
 
-// Configure middleware
+// Configure the Developer Exception Page
+if (app.Environment.IsDevelopment())
+{
+app.UseDeveloperExceptionPage();
+}
+else
+{
+app.UseExceptionHandler("/Home/Error");
+// The default HSTS value is 30 days.
 app.UseHsts();
+}
+
+// Configure middleware
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+
 app.UseRouting();
+
+app.UseAuthentication(); 
 app.UseAuthorization();
 
 app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+name: "default",
+pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+
