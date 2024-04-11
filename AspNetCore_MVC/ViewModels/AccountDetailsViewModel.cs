b5793011c62beed5b1395bba9ca7866477905058
LucasWictor <Dahlulle@gmail.com>
2024-04-11 13:26:13 +0200

    Removed hardcoded values

--- AspNetCore_MVC/ViewModels/AccountDetailsViewModel.cs
+++ AspNetCore_MVC/ViewModels/AccountDetailsViewModel.cs
@@ -5,14 +5,8 @@ namespace AspNetCore_MVC.ViewModels
     public class AccountDetailsViewModel
     {
         public string Title { get; set; } = "Account Details";
-        public AccountDetailsBasicInfoModel BasicInfo { get; set; } = new AccountDetailsBasicInfoModel()
-        {
-            ProfileImage = "images/avatar.svg",
-            FirstName = "Lucas",
-            LastName = "Wictor",
-            Email = "Lucas@domain.com"
-        };
-        public AccountDetailsAdressInfoModel AddressInfo { get; set; } = new AccountDetailsAdressInfoModel();
-
+        public AccountDetailsBasicInfoModel BasicInfo { get; set; }
+        public AccountDetailsAddressInfoModel AddressInfo { get; set; }
     }
+
 }
