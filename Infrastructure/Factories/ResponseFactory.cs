39632283d2c0d12cd2b3f8485792a87edd8bc87d
LucasWictor <Dahlulle@gmail.com>
2024-04-11 13:28:03 +0200

    Updated ResponseFactory

--- Infrastructure/Factories/ResponseFactory.cs
+++ Infrastructure/Factories/ResponseFactory.cs
@@ -1,62 +1,65 @@
 using Infrastructure.Models;
 
-namespace Infrastructure.Factories;
-
-public class ResponseFactory
+namespace Infrastructure.Factories
 {
-    //ok
-    public static ResponseResult Ok()
+    public class ResponseFactory
     {
-        return new ResponseResult
+        //ok
+        public static ResponseResult Ok()
         {
-            Message ="Succeeded",
-            StatusCode = StatusCodes.Ok 
-        };
-    }
-    //custom message
-    public static ResponseResult Ok(string? message = null)
-    {
-        return new ResponseResult
+            return new ResponseResult
+            {
+                Message = "Succeeded",
+                StatusCode = StatusCodes.Ok
+            };
+        }
+
+        //custom message
+        public static ResponseResult Ok(string? message = null)
         {
-            Message = message ?? "Succeeded",
-            StatusCode = StatusCodes.Ok 
-        };
-    }
-    //object + custom message
-    public static ResponseResult Ok(object obj, string? message = null)
-    {
-        return new ResponseResult
+            return new ResponseResult
+            {
+                Message = message ?? "Succeeded",
+                StatusCode = StatusCodes.Ok
+            };
+        }
+
+        //object + custom message
+        public static ResponseResult Ok(object obj, string? message = null)
         {
-            ContentResult = obj,
-            Message = message ?? "Succeeded",
-            StatusCode = StatusCodes.Ok 
-        };
-    }
-    
-    public static ResponseResult Error(string? message = null)
-    {
-        return new ResponseResult
+            return new ResponseResult
+            {
+                ContentResult = obj,
+                Message = message ?? "Succeeded",
+                StatusCode = StatusCodes.Ok
+            };
+        }
+
+        public static ResponseResult Error(string? message = null, StatusCodes statusCode = StatusCodes.Error)
         {
-            Message = message ?? "Failed",
-            StatusCode = StatusCodes.Error 
-        };
-    }
-    
-    public static ResponseResult Exists(string? message = null)
-    {
-        return new ResponseResult
+            return new ResponseResult
+            {
+                Message = message ?? "Failed",
+                StatusCode = statusCode
+            };
+        }
+
+        public static ResponseResult Exists(string? message = null)
         {
-            Message = message ?? "Already exists",
-            StatusCode = StatusCodes.Exists 
-        };
-    }
-    
-    public static ResponseResult NotFound(string? message = null)
-    {
-        return new ResponseResult
+            return new ResponseResult
+            {
+                Message = message ?? "Already exists",
+                StatusCode = StatusCodes.Exists
+            };
+        }
+
+        public static ResponseResult NotFound(string? message = null)
         {
-            Message = message ?? "Not found",
-            StatusCode = StatusCodes.Notfound 
-        };
+            return new ResponseResult
+            {
+                Message = message ?? "Not found",
+                StatusCode = StatusCodes.Notfound
+            };
+        }
     }
 }
