5eb61c21772fc06ffd45ebfd81b781e32b75ea22
LucasWictor <Dahlulle@gmail.com>
2024-04-11 13:28:22 +0200

    Added logging and catch exceptions

--- Infrastructure/Services/UserService.cs
+++ Infrastructure/Services/UserService.cs
@@ -1,62 +1,72 @@
 using Infrastructure.Entities;
-using Infrastructure.Factories;
 using Infrastructure.Helpers;
 using Infrastructure.Models;
 using Infrastructure.Repositories;
 using Microsoft.Extensions.Logging;
-
+using Infrastructure.Factories;
 
 namespace Infrastructure.Services
 {
     public class UserService
     {
         private readonly UserRepository _repository;
-        private readonly AddressService _addressService;
         private readonly ILogger<UserService> _logger;
 
-        public UserService(UserRepository repository, AddressService addressService, ILogger<UserService> logger)
+        public UserService(UserRepository repository, ILogger<UserService> logger)
         {
             _repository = repository;
-            _addressService = addressService;
             _logger = logger;
         }
 
         public async Task<ResponseResult> CreateUserAsync(SignUpModel model)
         {
-            var existsResult = await _repository.AlreadyExistsAsync(x => x.Email == model.Email);
-            if (existsResult.StatusCode == StatusCodes.Exists)
+            try
             {
-                return ResponseFactory.Error("User already exists");
+                var existsResult = await _repository.AlreadyExistsAsync(x => x.Email == model.Email);
+                if (existsResult.StatusCode == Infrastructure.Models.StatusCodes.Ok)
+                {
+                    return ResponseFactory.Error("User already exists", Infrastructure.Models.StatusCodes.Exists);
+                }
+
+                var userEntity = UserFactory.Create(model);
+                var createResult = await _repository.CreateOneAsync(userEntity);
+
+                if (createResult.StatusCode != Infrastructure.Models.StatusCodes.Ok)
+                {
+                    return createResult;
+                }
+
+                return ResponseFactory.Ok("User was created successfully");
             }
-
-            var userEntity = UserFactory.Create(model); 
-            var createResult = await _repository.CreateOneAsync(userEntity);
-
-            if (createResult.StatusCode != StatusCodes.Ok)
+            catch (Exception ex)
             {
-                return createResult;
+                _logger.LogError(ex, "Failed to create user");
+                return ResponseFactory.Error("Failed to create user", Infrastructure.Models.StatusCodes.Error);
             }
-
-            return ResponseFactory.Ok("User was created successfully");
         }
-        
+
         public async Task<ResponseResult> SignInUserAsync(SignInModel model)
         {
             try
             {
                 var result = await _repository.GetOneAsync(x => x.Email == model.Email);
-                if (result.StatusCode == StatusCodes.Ok && result.ContentResult != null)
+                if (result.StatusCode == Infrastructure.Models.StatusCodes.Ok && result.ContentResult != null)
                 {
                     var userEntity = (UserEntity)result.ContentResult;
-                    
+
                     if (PasswordHasher.ValidateSecurePassword(model.Password, userEntity.Password,
                             userEntity.SecurityKey))
                         return ResponseFactory.Ok();
                 }
-                
-                return ResponseFactory.Error("Incorrect email or password");
+
+                return ResponseFactory.Error("Incorrect email or password", Infrastructure.Models.StatusCodes.Error);
             }
-            catch (Exception ex) { return ResponseFactory.Error(ex.Message); }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Failed to sign in user");
+                return ResponseFactory.Error("Failed to sign in user", Infrastructure.Models.StatusCodes.Error);
+            }
         }
     }
 }
+
